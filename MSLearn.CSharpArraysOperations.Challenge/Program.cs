c7664d6908fe3ca940e9f7d3f161f81ade31e2d8
jra <julien.gault.50@gmail.com>
2024-03-06 18:06:51 +0100

    solution

--- MSLearn.CSharpArraysOperations.Challenge/Program.cs
+++ MSLearn.CSharpArraysOperations.Challenge/Program.cs
@@ -1,1 +1,30 @@
 ﻿string pangram = "The quick brown fox jumps over the lazy dog";
+
+/*
+Write the code necessary to reverse the letters of each word in place and display the result.
+
+In other words, don't just reverse every letter in the variable pangram. Instead, you'll need to reverse just the letters in each word, but print the reversed word in its original position in the message.
+
+Your code must produce the following output:
+ehT kciuq nworb xof spmuj revo eht yzal god
+*/
+
+string[] words = pangram.Split(' ');
+string[] reversedPangramWords = new string[words.Length];
+
+/*
+ * For each word string in the pangram
+ * we convert the word to an array of letters
+ * we revert that array of letter
+ * we convert that array of letter back to a string
+ * we add that string to our new array of words reversedPangramWords
+ * at the end, we print the new array of words reversedPangramWords with a join space, in order to put a space between each word
+ * */
+for (int i = 0; i < words.Length; i++)
+{
+    char[] wordArray = words[i].ToCharArray();
+    Array.Reverse(wordArray);
+    reversedPangramWords[i] = new string(wordArray);
+}
+
+Console.WriteLine(string.Join(" ", reversedPangramWords));
